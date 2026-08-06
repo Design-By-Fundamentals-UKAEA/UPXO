2cb910effc471dfb06398424524e447570255016
Sunil Anandatheertha <66170566+SunilAnandatheertha@users.noreply.github.com>
2026-08-06 10:20:09 +0100

    docs(interfaces/moose): mark package as unimplemented stub

---
+++ src/upxo/interfaces/moose/__init__.py
@@ -0,0 +1,7 @@
+"""
+MOOSE interface package (stub).
+
+No MOOSE import/export implementation is provided in this package yet.
+Prefer VTK / Abaqus / NumPy export paths under ``upxo.meshing`` and
+``upxo.interfaces.io``.
+"""
