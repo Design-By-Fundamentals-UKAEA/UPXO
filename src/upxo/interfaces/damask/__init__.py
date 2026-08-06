7323dd4cf19a6fbabedb4fe61542dd8379bc10c7
Sunil Anandatheertha <66170566+SunilAnandatheertha@users.noreply.github.com>
2026-08-06 10:20:07 +0100

    docs(interfaces/damask): mark package as unimplemented stub

---
+++ src/upxo/interfaces/damask/__init__.py
@@ -0,0 +1,7 @@
+"""
+DAMASK interface package (stub).
+
+No DAMASK import/export implementation is provided in this package yet.
+Prefer VTK / Abaqus / NumPy export paths under ``upxo.meshing`` and
+``upxo.interfaces.io``.
+"""
