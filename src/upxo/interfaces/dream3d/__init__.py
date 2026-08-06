1f8b97ef42651aef3326bb58e5a27825d0a1d9a8
Sunil Anandatheertha <66170566+SunilAnandatheertha@users.noreply.github.com>
2026-08-06 10:20:10 +0100

    docs(interfaces/dream3d): mark package as unimplemented stub

---
+++ src/upxo/interfaces/dream3d/__init__.py
@@ -0,0 +1,7 @@
+"""
+Dream.3D interface package (stub).
+
+No Dream.3D import/export implementation is provided in this package yet.
+Prefer VTK / Abaqus / NumPy export paths under ``upxo.meshing`` and
+``upxo.interfaces.io``.
+"""
